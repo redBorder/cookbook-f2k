5e12ef69547584aa5926aafe575d95fe98e80e71
Miguel Negrón <45871721+manegron@users.noreply.github.com>
2024-05-26 09:55:17 +0100

    Update metadata.rb

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -1,7 +1,6 @@
 name             'f2k'
-maintainer       'Alberto Rodriguez'
-maintainer_email 'arodriguez@redborder'
-license          'AGPL v3'
+maintainer       'Eneo Tecnología S.L.'
+maintainer_email 'git@redborder.com'
+license          'AGPL-3.0'
 description      'Installs/Configures cookbook-f2k'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.8'
