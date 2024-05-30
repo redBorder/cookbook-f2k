7b47f64b740f008754edc4fc4d8817ed89fbad5c
Miguel Negron <manegron@redborder.com>
2024-05-30 16:20:52 +0100

    Lint helper 2

--- resources/libraries/helper.rb
+++ resources/libraries/helper.rb
@@ -14,15 +14,11 @@ module F2k
     def get_objects(object_type)
       known_ips = get_known_ips
       objects = {}
-      if (node['redborder'] && 
-          node['redborder']['objects'] && 
-          node['redborder']['objects'][object_type] && 
-          node['redborder']['objects'][object_type].class != Chef::Node::ImmutableArray)
-        if object_type == 'hosts'
-          objects = known_ips.merge(node['redborder']['objects'][object_type])
-        else
-          objects = node['redborder']['objects'][object_type]
-        end
+      if node['redborder'] &&
+         node['redborder']['objects'] &&
+         node['redborder']['objects'][object_type] &&
+         node['redborder']['objects'][object_type].class != Chef::Node::ImmutableArray
+        objects = (object_type == 'hosts') ? known_ips.merge(node['redborder']['objects'][object_type]) : node['redborder']['objects'][object_type]
       elsif object_type == 'hosts'
         objects = known_ips
       end
